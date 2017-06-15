a36320dd97f22357c1f79fda7fdaf41608c2a38c
ronanlima <7xhy#23>
2017-06-15 12:55:51 -0300

    Inclusão de fab com ação de apagar o desenho

--- app/src/main/java/home/com/br/recadastramento/MainActivity.java
+++ app/src/main/java/home/com/br/recadastramento/MainActivity.java
@@ -3,7 +3,9 @@ package home.com.br.recadastramento;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.util.DisplayMetrics;
+import android.view.View;
 
 import home.com.br.recadastramento.view.PaintView;
 
@@ -14,14 +16,23 @@ import home.com.br.recadastramento.view.PaintView;
 public class MainActivity extends Activity {
 
     private PaintView paintView;
+    private FloatingActionButton fabEraser;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         paintView = (PaintView) findViewById(R.id.paintview);
+        fabEraser = (FloatingActionButton) findViewById(R.id.fab_eraser);
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         paintView.init(metrics);
+
+        fabEraser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                paintView.clear();
+            }
+        });
     }
 }
